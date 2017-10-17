ebab27cd7c333763a6f9b57b0fa928e967144780
sabrinaboudjedar <fs_boudjedar@esi.dz>
2017-10-17 11:17:30 +0200

    Ajout d'une classe d'exception appelet dans la somme

---
+++ src/igl_tp/Exeption.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package igl_tp;
+
+/**
+ *
+ * @author khaled_mhz
+ */
+public class Exeption extends Exception{
+  @Override
+    public String getMessage() {
+        super.getMessage();
+        return ("la taille des tableaux n'est pas la meme");  
+}}
