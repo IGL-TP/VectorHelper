23a2f40f0f635f3bc14da00a7f6e07c139ee967c
sabrinaboudjedar <fs_boudjedar@esi.dz>
2017-10-17 19:33:42 +0100

    rennomer la class d'excption : DiffifrenteTailleException

---
+++ src/igl_tp/DiffirenteTailleException.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package igl_tp;
+
+/**
+ *
+ * @author khaled_mhz
+ */
+public class DiffirenteTailleException extends Exception{
+
+    public DiffirenteTailleException() {
+        System.out.println("la taille des tableaux n'est pas la meme");
+    }
+    @Override
+    public String getMessage() {
+        super.getMessage();
+        return ("la taille des tableaux n'est pas la meme");  
+    }
+}
